5ac8cfe06338a3a6408abe666cbe511735adf7b2
Filip Smets <alemaaltevinden@gmail.com>
2014-07-26 23:42:38 +0200

    Added example html file + finished context

--- TiledCanvas.js
+++ TiledCanvas.js
@@ -3,19 +3,33 @@ function TiledCanvas (canvas, settings) {
     this.ctx = this.canvas.getContext('2d');
     this.leftTopX = 0;
     this.leftTopY = 0;
-    this.zoom = 1;
+    this.affecting = [[0, 0], [0, 0]];
     this.chunks = {};
     this.settings = this.normalizeDefaults(settings, this.defaulSettings);
-    this.actionQueue = [];
+    this.contextQueue = [];
+    this.contextPropertys = {};
+    this.context = this.createContext();
 }
 
 TiledCanvas.prototype.defaultSettings = {
-    chunkSize: 400
+    chunkSize: 256
+};
+
+TiledCanvas.prototype.cloneObject = function (obj) {
+	var clone = {};
+	for (var k in obj) {
+		if (typeof obj[k] === "object" && !(obj[k] instanceof Array)) {
+			clone[k] = this.cloneObject(obj[k]);
+		} else {
+			clone[k] = obj[k]
+		}
+	}
+	return clone;
 };
 
 TiledCanvas.prototype.normalizeDefaults = function normalizeDefaults (target, defaults) {
 	target = target || {};
-	var normalized = utils.cloneObject(target);
+	var normalized = this.cloneObject(target);
 	for (var k in defaults) {
 		if (typeof defaults[k] === "object" && !(defaults[k] instanceof Array)) {
 			normalized[k] = this.normalizeDefaults(target[k] || {}, defaults[k]);
@@ -44,19 +58,15 @@ TiledCanvas.prototype.zoom = function zoom (zoom) {
     this.redraw();
 };
 
-TiledCanvas.prototype.addToQueue = function addToQueue () {
-
-};
-
 TiledCanvas.prototype.execute = function execute () {
 
 };
 
 TiledCanvas.prototype.drawingRegion = function (startX, startY, endX, endY) {
-    this.affecting = [
-        [startX, startY],
-        [endX, endY]
-    ];
+    this.affecting[0][0] = startX;
+    this.affecting[0][1] = startY;
+    this.affecting[1][0] = endX;
+    this.affecting[1][1] = endY;
 };
 
 TiledCanvas.prototype.newCtx = function newCtx (width, height) {
@@ -66,13 +76,25 @@ TiledCanvas.prototype.newCtx = function newCtx (width, height) {
     return ctx;
 };
 
-TiledCanvas.prototype.context = (function () {
+TiledCanvas.prototype.createContext = function createContext () {
     var context = {};
     var ctx = document.createElement('canvas').getContext('2d');
     for (var key in ctx) {
         if (typeof ctx[key] === 'function') {
-            
+            context[key] = function () {
+                this.contextQueue.push([key].concat(arguments));
+            }.bind(this);
+        } else if (typeof ctx[key] !== 'object') {
+            this.contextPropertys[key] = ctx[key];
+
+            context.__defineGetter__(key, function () {
+                return this.contextPropertys[key];
+            }.bind(this));
+
+            context.__defineSetter__(key, function (value) {
+                return this.contextPropertys[key] = value;
+            }.bind(this));
         }
     }
     return context;
-})();
+};
